b3366f99c8fef324e11f7361f4ce8b17c17eb3df
unknown <franciscosanchezdelatorre@gmail.com>
2019-01-24 00:14:24 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-
-    </div>
+    <bar-nav></bar-nav>
     <router-view/>
+
+    
+
   </div>
 </template>
+
+<script>
+  import BarNav from '@/components/BarNav'
+
+  export default {
+    name:'app',
+    components:{
+      BarNav
+    }
+  }
+</script>
+
 
 <style>
 #app {
